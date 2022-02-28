849715a16a73d5b393048fa7e072e1cf79990b31
Arumugam J <sixface@msn.com>
2022-02-27 23:37:15 -0500

    Adding Process listing command
    The UI now renders the device list and the process list.

--- src/main/kotlin/com/wbtwzd/logdog/ui/MainWindow.kt
+++ src/main/kotlin/com/wbtwzd/logdog/ui/MainWindow.kt
@@ -23,25 +23,94 @@
 package com.wbtwzd.logdog.ui
 
 import androidx.compose.desktop.ui.tooling.preview.Preview
-import androidx.compose.material.Button
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.wbtwzd.logdog.app.AppState
+import com.wbtwzd.logdog.command.Device
+import com.wbtwzd.logdog.command.Process
+import com.wbtwzd.logdog.command.DeviceListCmd
+import com.wbtwzd.logdog.command.ProcessListCmd
+import com.wbtwzd.logdog.util.Log
+import kotlinx.coroutines.launch
 
 @Composable
 @Preview
 fun App() {
-    var text by remember { mutableStateOf("Hello, World!") }
+
+    val appState = remember { AppState() }
 
     MaterialTheme {
-        Button(onClick = {
-            text = "Hello, Desktop!"
-        }) {
-            Text(text)
+        Column(modifier = Modifier.padding(10.dp)) {
+            ToolBar(appState.selectedDevice, appState.selectedProcess)
+
         }
     }
 }
+
+@Preview
+@Composable
+fun ToolBar(selectedDevice: MutableState<Device?>, selectedProcess: MutableState<Process?>) {
+
+    var devices: List<Device> by remember { mutableStateOf(emptyList()) }
+    var processes: List<Process> by remember { mutableStateOf(emptyList()) }
+    val localScope = rememberCoroutineScope()
+    localScope.launch {
+        DeviceListCmd().execute()?.let {
+            devices = it
+//            if (it.isNotEmpty()) selectedDevice.value = it[0]
+        }
+    }
+
+    Surface(
+        shape = MaterialTheme.shapes.medium,
+        modifier = Modifier.fillMaxWidth().height(200.dp),
+        color = MaterialTheme.colors.secondary
+    ) {
+        Column(
+            modifier = Modifier.padding(25.dp).fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Row(modifier = Modifier.fillMaxWidth()) {
+                DropDownSelect(
+                    devices, selected = selectedDevice,
+                    itemLabelBuilder = { it?.name ?: it?.serialName ?: "Select a device" },
+                    modifier = Modifier.width(250.dp).padding(5.dp),
+                    onSelect = { dev ->
+                        dev?.let {
+                            localScope.launch {
+                                ProcessListCmd(it).execute()?.let { pro ->
+                                    log.i("processes = $pro")
+                                    processes = pro.sortedBy { it.name }
+                                }
+                            }
+                        }
+                    }
+                )
+                DropDownSelect(
+                    processes, selected = selectedProcess,
+                    itemLabelBuilder = { it?.name ?: "Select a Process" },
+                    modifier = Modifier.width(250.dp).padding(5.dp)
+                )
+            }
+        }
+    }
+}
+
+private val log = Log("Main Layout")
